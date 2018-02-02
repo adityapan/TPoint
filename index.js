8f5ea32e6e3b373cf2df2c9cad47051d3c0d1003
vorTex <pan.aditya93@gmail.com>
2018-02-02 16:55:24 +0800

    added pug for templating

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 var express = require('express');
 var app = express();
+app.set('view engine', 'pug');
+app.set('views','./views');
 
 var things = require('./things.js');
 
 app.use('/things', things);
 
 
+app.get('/first_template', function(req, res){
+   res.render('first_view');
+});
+
+
+app.get('*', function(req,res){
+	res.send("Invalid Url");
+});
 
 // app.get('/', function(req, res){
 // 	res.send("Hello World!");
--- things.js
+++ things.js
@@ -3,14 +3,13 @@ var router = express.Router();
 
 
 
-router.get('/', function(req, res){
-	res.send("Get Routes and Shit");
+router.get('/:names/:id([0-9]{5})', function(req, res){
+	res.send("Get "+req.params.names+" and "+req.params.id);
 });
 
 router.post('/', function(req, res){
 	res.send("Post Routes and Shit");
 });
-
 
 
 module.exports = router; 
